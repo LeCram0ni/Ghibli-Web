28567ee443a410938cde5d6a76a7384f0bea54af
marcel <15247414+LeCram0ni@users.noreply.github.com>
2024-08-01 22:44:22 +0200

    gallery working

--- app.js
+++ app.js
@@ -22,3 +22,33 @@ function start() {
 window.addEventListener("load", updateWidth);
 window.addEventListener("resize", updateWidth);
 
+window.onload = start;
+
+function start() {
+  
+  $("#left-button").on("click", handleLeftClick);
+  $("#right-button").on("click", handleRightClick);
+}
+ 
+
+function handleLeftClick() {
+    $("#left-button").off("click");
+    $(".carousel").animate({
+      left: "-" + "${output}" + "px"
+    },400,"swing", function complete(){
+      $(".carousel img:first-child").appendTo($(".carousel"));
+      $(".gcarousel").css("left", "0");
+      $("#left-button").on("click", handleLeftClick);
+    });
+}
+
+function handleRightClick() {
+  $("#right-button").off("click");
+    $(".carousel img:last-child").prependTo($(".carousel"));
+    $(".carousel").css("left", "-" + $(".carousel img:first-child").width() + "px");
+    $(".carousel").animate({
+      left: 0
+    },400,"swing", function complete(){
+      $("#right-button").on("click", handleRightClick);
+    });  
+}
